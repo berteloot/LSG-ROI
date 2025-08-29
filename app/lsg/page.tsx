572fbafb50cd47b49579f695dcfc86d8bcb7eebc
berteloot <stan@altilead.com>
2025-08-29 15:49:25 -0400

    Remove B11 and LSG pages - clean up redundant routes

--- app/lsg/page.tsx
+++
@@ -1,9 +0,0 @@
-import LSGCalculator from '@/components/LSGCalculator'
-
-export default function LSGPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <LSGCalculator />
-    </div>
-  )
-}
